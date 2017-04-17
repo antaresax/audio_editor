896759cea9b4789573680396d2883db7fea7f313
marte <marte03@KANG-Hyoseok.local>
2017-04-17 17:50:59 +0900

    new

---
+++ js/Package.js
@@ -0,0 +1,23 @@
+/**
+ * PM
+ * @Package
+ * @desc : PM Package Definition
+ *
+ */
+
+// PM Declare
+PM 	= {};
+
+// Engine Namespace
+PM.Engine = {};
+
+// Front Namespace
+PM.Front = {
+
+	// 앱 Class
+	App : {}
+
+	// 현재 실행중인 앱
+	, Working : {}
+
+};
